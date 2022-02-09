9fdc1911177a0b1f996c3b134726b90c77d67471
kefeh <kefehcollins@gmail.com>
2022-02-09 03:12:12 +0100

    adds the persistent puzzle name to all the screens

--- lib/theme/widgets/puzzle_name.dart
+++ lib/theme/widgets/puzzle_name.dart
@@ -1,8 +1,12 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:spozzle/layout/layout.dart';
-import 'package:spozzle/theme/theme.dart';
-import 'package:spozzle/typography/typography.dart';
+import '../../l10n/l10n.dart';
+
+import '../../layout/layout.dart';
+import '../../typography/typography.dart';
+import '../theme.dart';
 
 /// {@template puzzle_name}
 /// Displays the name of the current puzzle theme.
@@ -20,21 +24,30 @@ class PuzzleName extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final PuzzleTheme theme = context.select((ThemeBloc bloc) => bloc.state.theme);
+    final PuzzleTheme theme =
+        context.select((ThemeBloc bloc) => bloc.state.theme);
     final Color nameColor = color ?? theme.nameColor;
 
     return ResponsiveLayoutBuilder(
-      small: (context, child) => const SizedBox(),
-      medium: (context, child) => const SizedBox(),
-      large: (context, child) => AnimatedDefaultTextStyle(
-        style: PuzzleTextStyle.headline5.copyWith(
-          color: nameColor,
-        ),
-        duration: PuzzleThemeAnimationDuration.textStyle,
-        child: Text(
-          theme.name,
-          key: const Key('puzzle_name_theme'),
-        ),
+      small: (BuildContext context, Widget? child) =>
+          Center(child: puzzleName(context, nameColor, theme.name)),
+      medium: (BuildContext context, Widget? child) =>
+          Center(child: puzzleName(context, nameColor, theme.name)),
+      large: (BuildContext context, Widget? child) =>
+          puzzleName(context, nameColor, theme.name),
+    );
+  }
+
+  AnimatedDefaultTextStyle puzzleName(
+      BuildContext context, Color nameColor, String name) {
+    return AnimatedDefaultTextStyle(
+      style: PuzzleTextStyle.headline5.copyWith(
+        color: nameColor,
+      ),
+      duration: PuzzleThemeAnimationDuration.textStyle,
+      child: Text(
+        context.l10n.puzzleChallengeName,
+        key: const Key('puzzle_name_theme'),
       ),
     );
   }
